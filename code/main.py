333773a4f73ce29965b5217517c7e45c879e6bfd
Subhamoy Karmakar <subhamoykarmakar224@gmail.com>
2022-02-21 15:35:50 -0800

    UI Setup

--- code/main.py
+++ code/main.py
@@ -1,11 +1,4 @@
 from app.app import app
-# import sys
-#
-#
-# def add_modules():
-#     sys.path.insert(0, './app/utils')
-#
 
 if __name__ == '__main__':
-    # add_modules()
     app.run(debug=True)
