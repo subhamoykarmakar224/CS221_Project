41a30ba2eabbcd064ea5b2c280317e5ca1c8ccb0
Ryan Miranda <ryanm7082@gmail.com>
2022-02-23 18:50:18 -0800

    added coding for pickling the index and auxiliary map

--- code/indexer/build_index.py
+++ code/indexer/build_index.py
@@ -3,12 +3,13 @@ import json
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from bs4 import BeautifulSoup
+import pickle
 
 
 def make_index():
     stop_list = set(stopwords.words('english'))
 
-    # TODO: modify start directory as needed for your machines
+    # TODO: modify start directory as needed for your machines and change to DEV
     start_directory = "../../../ANALYST"
 
     # structure of index is: {term : {docId :  frequency of term in doc} }
@@ -51,14 +52,42 @@ def make_index():
                     docId += 1
 
                     # can track progress or return early to see indexer working as expected
-                    if docId % 50 == 0:
-                        print(inverted_index)
-                        print(aux_map)
-                        # return
-    
+                    if docId % 100 == 0:
+                        print(len(inverted_index))
+                        print(docId)
+           
             except Exception as e:
                 print(e)
-       
+
+    # pickle the inverted_index and aux_map
+    with open('indexPickle', 'ab') as index_file:
+        pickle.dump(inverted_index, index_file, protocol=-1)                     
+    
+    with open('auxMap_pickle', 'ab') as aux_file:
+        pickle.dump(aux_map, aux_file, protocol=-1)   
+
+
+def print_index():
+    with open('auxMap_Pickle', 'rb') as aux_file:
+        aux_map = pickle.load(aux_file)
+
+        print(f'{len(aux_map)} documents')
+
+        for docId in aux_map:
+            print(f'{docId} : aux_map[docId]')
+    
+    with open('indexPickle', 'rb') as index_file:
+        inverted_index = pickle.load(index_file, encoding="bytes")
+
+        print(f'{len(inverted_index)} terms')
+
+        for term in sorted(inverted_index, key = lambda t: len(inverted_index[t])):
+            print(f'{term} : {inverted_index[term]}')    
+           
 
 if __name__ == '__main__':
     make_index()
+    #print_index()
+
+
+
