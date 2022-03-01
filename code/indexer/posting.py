a01306649510174f594e774022ec82cf4411ea7b
Ryan Miranda <ryanm7082@gmail.com>
2022-03-01 11:53:06 -0800

    deleted files

--- code/indexer/posting.py
+++
@@ -1,8 +0,0 @@
-class Posting:
-
-    def __init__(self, docId, frequency ) -> None:
-        self.docId = docId
-        self.frequency = frequency
-
-    def __hash__(self) -> int:
-        return hash(self.docId)
