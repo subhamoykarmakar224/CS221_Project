bf7f1ac922560444c559410eef2eca86aa32d657
Ryan Miranda <ryanm7082@gmail.com>
2022-03-03 13:42:36 -0800

    updated UI to show tags

--- code/app/utils/Database.py
+++ code/app/utils/Database.py
@@ -1,3 +1,4 @@
+from socketserver import ThreadingUDPServer
 from simple_attempt.search.boolean_query import BooleanQuery
 
 
@@ -6,7 +7,12 @@ def get_data(prefix=''):
     found_docs = BQ.search(prefix)
 
     res = []
+    tags = ''
+    for token in prefix.split(' '):
+        if len(token) > 2:
+            tags += ' #' + token
+
     for i in range(len(found_docs)):
-        res.append({ 'title': i+1, 'tags': '', 'last_updated': '', 'url': found_docs[i][0] })
+        res.append({ 'title': i+1, 'tags': tags, 'last_updated': 'XX', 'url': found_docs[i][0] })
 
     return res
